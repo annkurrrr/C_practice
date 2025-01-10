d6ff2a1eb16fde199dbd9a4cc81b0bf3fb0aff94
annkurrrr <ankur.kunde@gmail.com>
2025-01-10 22:20:37 +0530

    length of longest consecutive sequence

---
+++ longest_sequence.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(){
+    int num,i,j,temp,count=1,lcount=1;
+    printf("Enter the number of elements in the array:");
+    scanf("%d",&num);
+    int arr[num];
+    for(i=0;i<num;i++){
+        printf("Enter the element at position %d:",i+1);
+        scanf("%d",&arr[i]);
+    }
+    printf("\nThe sorted array is:\n");
+    for(i=0;i<num;i++){
+        for(j=i+1;j<num;j++){
+            if(arr[i]>arr[j]){
+                temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        printf("%d ",arr[i]);
+    }
+    for(i=0;i<num;i++){
+        if(arr[i+1]-arr[i]==0 || arr[i+1]-arr[i]==1){
+            count++;
+        }
+        else{
+            if(count>lcount){
+                lcount=count;
+                i+=count-1;
+                count=1;
+            }
+        }
+    }
+    printf("\n\nLength of the longest consecutive secuence is %d.",lcount);
+    return 0;
+}
